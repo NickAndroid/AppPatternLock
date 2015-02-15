a3f097718311c576ec105efda299ac1e68c6f743
nick <nick.guo@zhntd.com>
2015-02-15 09:42:24 +0800

    Remove unused

--- AppPatternLock/src/dev/nick/apppatternlock/manager/AppLockManager.java
+++
@@ -1,9 +0,0 @@
-package dev.nick.apppatternlock.manager;
-
-import android.app.Activity;
-
-public class AppLockManager {
-
-	private Class<? extends Activity> targetActivity;
-
-}
--- AppPatternLock/src/dev/nick/apppatternlock/pattern/PatternMode.java
+++
@@ -1,5 +0,0 @@
-package dev.nick.apppatternlock.pattern;
-
-public enum PatternMode {
-	EDIT, UNLOCK
-}
